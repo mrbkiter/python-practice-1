a55de75920dd844b12cf1b8f927e83d7705d1a4a
Nguyen Yen Vu <vu.nguyen@hrboss.com>
2018-01-24 19:27:59 +0700

    implement wordcount + word top

--- basic/wordcount.py
+++ basic/wordcount.py
@@ -2,6 +2,9 @@
 # Copyright 2010 Google Inc.
 # Licensed under the Apache License, Version 2.0
 # http://www.apache.org/licenses/LICENSE-2.0
+from builtins import FileNotFoundError
+import operator
+from audioop import reverse
 
 # Google's Python Class
 # http://code.google.com/edu/languages/google-python-class/
@@ -39,19 +42,43 @@ print_words() and print_top().
 
 import sys
 
+def processFileWordsAndCount(filename):
+    try:
+        f = open(filename, 'rU')
+        words = {}
+        for line in f:   ## iterates over the lines of the file
+            for s in line.split():
+                key = s.lower()
+                words[key]= words.get(key, 0) + 1
+        f.close()
+    except FileNotFoundError:
+        print('file %s not found' % filename )
+    return words
 # +++your code here+++
 # Define print_words(filename) and print_top(filename) functions.
 # You could write a helper utility function that reads a file
 # and builds and returns a word/count dict for it.
 # Then print_words() and print_top() can just call the utility function.
+def print_words(filename):
+    words = processFileWordsAndCount(filename)
+    for key in sorted(words.keys()):
+        print(key, words[key])
 
+def print_top(filename, topNum):
+    #print('print_top %' % filename)
+    words = processFileWordsAndCount(filename)
+    sortedDict = sorted(words.items(), key=lambda k: k[1], reverse=True)
+    #print(sortedDict)
+    #words.items().
+    for i in range(topNum):
+        print(sortedDict[i])
 ###
 
 # This basic command line argument parsing code is provided and
 # calls the print_words() and print_top() functions which you must define.
 def main():
   if len(sys.argv) != 3:
-    print 'usage: ./wordcount.py {--count | --topcount} file'
+    print ('usage: ./wordcount.py {--count | --topcount} file')
     sys.exit(1)
 
   option = sys.argv[1]
@@ -59,10 +86,19 @@ def main():
   if option == '--count':
     print_words(filename)
   elif option == '--topcount':
-    print_top(filename)
+    print_top(filename, 20)
   else:
-    print 'unknown option: ' + option
+    print ('unknown option: ' + option)
     sys.exit(1)
 
 if __name__ == '__main__':
-  main()
+  #main()
+  #print_words('alice.txt')
+  
+  print_top('alice.txt', 20)
+  '''
+  f = open('alice.txt', 'rU')
+  for line in f:   ## iterates over the lines of the file
+    print (line)    ## trailing , so print does not add an end-of-line char
+                   ## since 'line' already includes the end-of line.
+  f.close()'''
