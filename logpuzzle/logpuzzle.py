9a130079757c3a2208768dc9d25a0e72f135bb18
Nguyen Yen Vu <vu.nguyen@hrboss.com>
2018-01-28 23:20:29 +0700

    complete puzzle exercise

--- logpuzzle/logpuzzle.py
+++ logpuzzle/logpuzzle.py
@@ -10,6 +10,7 @@ import os
 import re
 import sys
 import urllib
+from urllib.error import HTTPError
 
 """Logpuzzle exercise
 Given an apache logfile, find the puzzle urls and download the images.
@@ -25,8 +26,27 @@ def read_urls(filename):
   Screens out duplicate urls and returns the urls sorted into
   increasing order."""
   # +++your code here+++
+  FILE_NAME_PATTERN = '^\w+_(.*)$'
+  LOG_PATTERN = '(\d+\.\d+\.\d+\.\d+).* "GET\s+([^\s]+)\s+([^/]+)/.*\n'
+  match = re.search(FILE_NAME_PATTERN, filename)
+  host = ''
+  if match:
+      host = match.group(1)
+  else:
+      print('Filename doesnt comply pattern')
+      exit(1)
+  f = open(file=filename, mode='r')
+  tuples = re.findall(r'%s' % LOG_PATTERN, f.read())
+  urlDict = {}
+  for tuple in tuples:
+      
+      extractedUrl  = tuple[2].lower() + '://www.' + host + tuple[1]
+      count = urlDict.get(extractedUrl, 0)
+      if count > 0: 
+          print('fromip=%s url=%s' %(tuple[0], extractedUrl))
+      urlDict[extractedUrl] = count + 1;
+  return sorted(urlDict.keys())
   
-
 def download_images(img_urls, dest_dir):
   """Given the urls already in the correct order, downloads
   each image into the given directory.
@@ -36,13 +56,38 @@ def download_images(img_urls, dest_dir):
   Creates the directory if necessary.
   """
   # +++your code here+++
+  if os.path.exists(dest_dir) == False:
+      os.mkdir(dest_dir)
+  indexHtml = '''<verbatim>
+                <html>
+                <body>
+                %s
+                </body>
+                </html>''';
+  pathToImages = []
+  for idx in range(len(img_urls)):
+    urlToFile = img_urls[idx]
+    try:
+        filename = 'img%d' % idx
+        pathToFile = os.path.abspath(os.path.join(dest_dir, filename))
+        urllib.request.urlretrieve(url=urlToFile, filename=pathToFile);
+        pathToImages.append('<img src="%s">' % pathToFile)
+    except HTTPError:
+        print('url %s 404 not found' % urlToFile)
+  htmlForImages = ''.join(pathToImages)
+  indexHtml = indexHtml % htmlForImages
+  indexFile = os.path.join(dest_dir, 'index.html')
   
-
+  f = open(indexFile, 'w')
+  #os.write(indexFile, indexHtml)
+  print(indexHtml, file=f)  
+  f.close()
+  
 def main():
   args = sys.argv[1:]
 
   if not args:
-    print 'usage: [--todir dir] logfile '
+    print ('usage: [--todir dir] logfile ')
     sys.exit(1)
 
   todir = ''
@@ -51,11 +96,11 @@ def main():
     del args[0:2]
 
   img_urls = read_urls(args[0])
-
+  print(img_urls)
   if todir:
     download_images(img_urls, todir)
   else:
-    print '\n'.join(img_urls)
+    print ('\n'.join(img_urls))
 
 if __name__ == '__main__':
   main()
